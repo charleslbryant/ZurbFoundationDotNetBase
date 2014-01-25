2d04a18105678b65cdd2809446e220aaae38b201
Charles Bryant <charles@charlesbryant.com>
2014-01-25 17:07:46 -0500

    Adding project.

---
+++ src/application/ZurbFoundationDotNetBase/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ZurbFoundationDotNetBase
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+              "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+              "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/foundation").Include(
+              "~/Scripts/foundation/foundation.js",
+              "~/Scripts/foundation/foundation.*"));
+        }
+    }
+}
---
+++ src/application/ZurbFoundationDotNetBase/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ZurbFoundationDotNetBase.MvcApplication" Language="C#" %>
